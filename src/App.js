30a0c2e8f6c103f00b8b2c4b3771e66066da13f3
nstylo <niklas@stylianou.info>
2019-07-11 10:55:13 +0200

    Draft entry

--- src/App.js
+++ src/App.js
@@ -5,14 +5,115 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Tab extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <p>This is a test.</p>;
+  }
+}
+
+class TopBar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div />;
+  }
+}
+
+class TODOPane extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div />;
+  }
+}
+
+class DONEPane extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div />;
+  }
+}
+
+class Entry extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      entry: '',
+      tickable: false,
+      id: '',
+      color: '',
+      pos: ''
+    };
+
+    // bind context
+    this.handleChange = this.handleChange.bind(this);
+    this.handleKeyPressed = this.handleKeyPressed.bind(this);
+  }
+
+  // controlled input
+  handleChange(event) {
+    this.setState({
+      entry: event.target.value
+    });
+  }
+
+  handleKeyPressed(event) {
+    // if enter was pressed
+    if (event.keyCode === 13) {
+      // render differently, send to server
+      this.setState({
+        tickable: true
+      });
+    } else if (event.button === 0) {
+      this.setState({
+        tickable: false
+      });
+    }
+  }
+
+  render() {
+    if (this.state.tickable) {
+      return (
+        <div>
+          <p className="Entry" onClick={this.handleKeyPressed}>
+            {this.state.entry}
+          </p>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <form className="EntryForm">
+            <input
+              className="EntryInput"
+              value={this.state.entry}
+              onChange={this.handleChange}
+              onKeyDown={this.handleKeyPressed}
+            />
+          </form>
+        </div>
+      );
+    }
+  }
+}
+
 function App() {
   return (
     <div className="App">
+      {/* this is a test comment */}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          This is a 2nd test for react
-        </a>
+        <Entry />
+        <Entry />
+        <Entry />
       </header>
     </div>
   );
