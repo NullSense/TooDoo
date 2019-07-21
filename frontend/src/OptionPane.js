f273791e062a5838f8351a1d2fce5bf167929099
nstylo <niklas@stylianou.info>
2019-07-21 22:41:47 +0200

    Make Optionpane open on click

--- frontend/src/OptionPane.js
+++ frontend/src/OptionPane.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const OptionPane = props => {
-  return <div>OptionPane</div>;
-};
+// const OptionPane = props => {
+//   return (
+//     <div>
+//       <input type="checkbox" className="optionopen" />
+//     </div>
+//   );
+// };
+
+class OptionPane extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: undefined
+    };
+  }
+
+  togglePane() {
+    if (this.state.open === undefined) {
+      this.setState({ open: true });
+      return;
+    }
+    this.setState(prev => {
+      return { open: !prev.open };
+    });
+  }
+
+  render() {
+    return (
+      <div
+        className={this.state.open === true ? 'options open' : this.state.open === false ? 'options closed' : 'options'}
+      >
+        <input className="optionopener" type="checkbox" onClick={this.togglePane.bind(this)} />
+      </div>
+    );
+  }
+}
 
 export default OptionPane;
