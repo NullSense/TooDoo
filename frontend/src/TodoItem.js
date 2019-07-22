1088790142c2af62562d8a4eff56224d04797139
nstylo <niklas@stylianou.info>
2019-07-23 00:29:03 +0200

    Make TodoItem inherit from PureComponent
    autoimplements shouldComponentUpdate for performance improvement

--- frontend/src/TodoItem.js
+++ frontend/src/TodoItem.js
@@ -1,61 +1,68 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
-const TodoItem = props => {
-  let color;
-  if (props.done) {
-    color = 'checked';
-  } else {
-    color = props.color;
-  }
+class TodoItem extends PureComponent {
+  render() {
+    let color;
+    if (this.props.done) {
+      color = 'checked';
+    } else {
+      color = this.props.color;
+    }
 
-  let style;
-  let crossedOut = null;
-  switch (color) {
-    case 'default':
-      style = { color: '#36454f', backgroundColor: '#f8f8fa' };
-      break;
-    case 'red':
-      style = { color: '#36454f', backgroundColor: 'hsl(355,100%,77%)', borderColor: 'hsl(355,100%,67%)' };
-      break;
-    case 'crimson':
-      style = { color: '#36454f', backgroundColor: 'hsl(3,100%,82%)', borderColor: 'hsl(3,100%,72%)' };
-      break;
-    case 'orange':
-      style = { color: '#36454f', backgroundColor: 'hsl(24,100%,86%)', borderColor: 'hsl(24,100%,76%)' };
-      break;
-    case 'green':
-      style = { color: '#36454f', backgroundColor: 'hsl(83,55%,84%)', borderColor: 'hsl(83,55%,74%)' };
-      break;
-    case 'blue':
-      style = { color: '#36454f', backgroundColor: 'hsl(158,55%,78%)', borderColor: 'hsl(158,55%,68%)' };
-      break;
-    case 'checked':
-      style = { color: '#c0c2ce', backgroundColor: '#e5e6eb' };
-      crossedOut = { textDecoration: 'line-through' };
-      break;
-    default:
-      style = { color: '#36454f', backgroundColor: '#f8f8fa' };
-      break;
-  }
+    let style;
+    let crossedOut = null;
+    switch (color) {
+      case 'default':
+        style = { color: '#36454f', backgroundColor: '#f8f8fa' };
+        break;
+      case 'red':
+        style = { color: '#36454f', backgroundColor: 'hsl(355,100%,77%)', borderColor: 'hsl(355,100%,67%)' };
+        break;
+      case 'crimson':
+        style = { color: '#36454f', backgroundColor: 'hsl(3,100%,82%)', borderColor: 'hsl(3,100%,72%)' };
+        break;
+      case 'orange':
+        style = { color: '#36454f', backgroundColor: 'hsl(24,100%,86%)', borderColor: 'hsl(24,100%,76%)' };
+        break;
+      case 'green':
+        style = { color: '#36454f', backgroundColor: 'hsl(83,55%,84%)', borderColor: 'hsl(83,55%,74%)' };
+        break;
+      case 'blue':
+        style = { color: '#36454f', backgroundColor: 'hsl(158,55%,78%)', borderColor: 'hsl(158,55%,68%)' };
+        break;
+      case 'checked':
+        style = { color: '#c0c2ce', backgroundColor: '#e5e6eb' };
+        crossedOut = { textDecoration: 'line-through' };
+        break;
+      default:
+        style = { color: '#36454f', backgroundColor: '#f8f8fa' };
+        break;
+    }
 
-  return (
-    <li style={style} className="todoitem">
-      <button className="delete" onClick={props.delete}>
-        x
-      </button>
-      <div>
-        <input type="checkbox" className="checkitem" onClick={props.check} defaultChecked={props.done ? 'done' : ''} />
-        <label className="itemlabel" style={crossedOut}>
-          {props.entry}
-        </label>
-      </div>
-      <br />
-      <hr style={style} />
-      <footer className="itemfooter">{props.dateTime}</footer>
-    </li>
-  );
-};
+    return (
+      <li style={style} className="todoitem">
+        <button className="delete" onClick={this.props.delete}>
+          x
+        </button>
+        <div>
+          <input
+            type="checkbox"
+            className="checkitem"
+            onClick={this.props.check}
+            defaultChecked={this.props.done ? 'done' : ''}
+          />
+          <label className="itemlabel" style={crossedOut}>
+            {this.props.entry}
+          </label>
+        </div>
+        <br />
+        <hr style={style} />
+        <footer className="itemfooter">{this.props.dateTime}</footer>
+      </li>
+    );
+  }
+}
 
 /**
  * prop type validation
