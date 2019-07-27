1a340ede4bbdd3bb9beab921b7388aa22ea38b79
Aerial <matas234@gmail.com>
2019-07-27 18:31:15 +0200

    Remove unused import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import TodoList from './TodoList.js';
 import LoginPage from './LoginPage.js';
-import Registration from './Registration.js';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
