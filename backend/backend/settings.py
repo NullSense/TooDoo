1563930911adba9a69d9e02bc504a47970452981
Aerial <matas234@gmail.com>
2019-07-28 19:10:01 +0200

    Fix env var declarations, update requirements

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,9 +41,11 @@ else: # deployment env
     CSRF_COOKIE_SECURE = True
     SESSION_COOKIE_SECURE = True
     # email
-    EMAIL_HOST = [os.getenv('EMAIL_HOST')]
-    EMAIL_PORT = [os.getenv('EMAIL_PORT')]
+    EMAIL_HOST = os.getenv('EMAIL_HOST')
+    EMAIL_PORT = os.getenv('EMAIL_PORT')
     EMAIL_USE_TLS = True
+    EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 CSRF_COOKIE_NAME = "csrftoken"
 
